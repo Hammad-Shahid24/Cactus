8cb05f41b7fc02869022575e4cb6065cecf43b1d
Hammad-Shahid24 <apilex40@gmail.com>
2024-11-04 11:49:36 +0500

    added custom colors and fonts in the tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        colors: {
+          primary: "#1a202c", // Custom primary color
+          secondary: "#2d3748", // Custom secondary color
+          accent: "#38b2ac", // Custom accent color
+          customGray: "#718096", // Custom gray color
+        },
+        fontFamily: {
+          sans: ["Inter", "sans-serif"], // Custom sans-serif font
+          serif: ["Merriweather", "serif"], // Custom serif font
+          mono: ["Fira Code", "monospace"], // Custom monospace font
+        },
+      },
+    },
   },
   plugins: [],
 };
